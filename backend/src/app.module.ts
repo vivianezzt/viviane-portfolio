192b32b131acb57fa71391c7bfa5834c4b9aa290
viviane <vivianeaguiarc@outlook.com>
2025-04-02 10:07:27 -0300

    feat:(backend) criar modulos de projetos e tecnologias

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DbModule } from './db/db.module';
+import { ProjetoModule } from './projeto/projeto.module';
+import { TecnologiaModule } from './tecnologia/tecnologia.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, ProjetoModule, TecnologiaModule],
   controllers: [AppController],
 })
 export class AppModule {}
