5e7fee5c1ec1996137fee6f800c0fc8acf4cad50
Viviane <vivianeaguiarc@outlook.com>
2025-04-12 19:38:40 -0300

    feat(menu): adicionar animações de escala, translação e hover nos itens do menu para melhorar a experiência do usuário

--- frontend/src/components/shared/Menu.tsx
+++ frontend/src/components/shared/Menu.tsx
@@ -2,39 +2,46 @@
 
 import { usePathname } from "next/navigation"
 import Link from "next/link"
+import { FaWhatsapp } from "react-icons/fa"
 
 
 export default function Menu() {
     const caminho = usePathname()
 
     return (
-        <nav className="flex gap-6 ">
+        <nav className="flex gap-6">
             <MenuItem href="/" selecionado={caminho === "/"}>Início</MenuItem>
             <MenuItem href="/projeto/1" selecionado={caminho.startsWith("/projeto")}>Projetos</MenuItem>
-            <MenuItem
-                href="https://vivianeaguiar.com/contato"
-                selecionado={false}
-                novaAba={true}>
+            <Link
+                href="http://localhost:4000/contato"
+                target="_blank"
+                className="flex items-center gap-2 text-sm border-green-400 hover:text-white text-zinc-300 transition-all duration-300 hover:-translate-y-1 hover:scale-105"
+            >
+                <FaWhatsapp className="text-[#25D366] text-lg" />
                 Contato
-            </MenuItem>
-
+            </Link>
         </nav>
+
     )
 }
-
 function MenuItem(props: {
     href: string,
     children: React.ReactNode,
     selecionado?: boolean,
     novaAba?: boolean,
-}) {
+  }) {
     return (
-        <Link href={props.href} target={props.novaAba ? "_blank" : "_self"}>
-            <span
-                className={`flex items-center gap-2 text-sm border-green-400 hover:text-white ${props.selecionado ? "border-b-4 text-white" : "text-zinc-300"}`}
-            >
-                {props.children}
-            </span>
-        </Link>
-    )
-}
+      <Link 
+        href={props.href} 
+        target={props.novaAba ? "_blank" : "_self"}
+        className={`
+          flex items-center gap-2 text-sm border-green-400
+          hover:text-white ${props.selecionado ? "border-b-4 text-white" : "text-zinc-300"}
+          transition-all duration-300 hover:-translate-y-1 hover:scale-105
+        `}
+      >
+        {props.children}
+      </Link>
+    );
+  }
+  
