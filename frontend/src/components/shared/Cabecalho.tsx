c8f56db4704db3d93cedfc8165c7e0beafeb4f74
Viviane <vivianeaguiarc@outlook.com>
2025-04-12 18:50:39 -0300

    feat(menu): adicionar navegação entre páginas e link para LinkedIn no Cabecalho

--- frontend/src/components/shared/Cabecalho.tsx
+++ frontend/src/components/shared/Cabecalho.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
 import Container from "./Container";
 import Link from "next/link";
+import Menu from "./Menu";
 
 export default function Cabecalho() {
     return (
-        <header className="flex items-center h-16 justify-between">
-            <Container className="flex-1">
-                <div>
-                    <Link href={"/"} className="hidden sm:block">
-                        <Image src="/logo.png" alt="Logo" width={80} height={0} />
+        <header className="w-full flex items-center h-16 bg-black/50">
+            <Container className="flex-1 flex justify-between items-center">
+                <div className="flex items-center gap-10">
+                    <Link href="/" className="hidden sm:block">
+                        <Image src="/logo.png" alt="Logo" width={80} height={0} className="cursor-pointer" />
                     </Link>
+                    <Menu />
+                </div>
+                <div className="hidden sm:flex items-center">
+                    <Link href="https://www.linkedin.com/in/vivianezzt/" target="_blank" className="bg-purple-700 rounded-full px-7 py-1 text-sm font-bold">Perfil</Link>
                 </div>
             </Container>
         </header>
